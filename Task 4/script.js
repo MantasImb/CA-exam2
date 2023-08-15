6c4cbd3c688386e8d4956008c1b5e237f89f75ea
Mantas Imbrasas <mantasimbrasas@gmail.com>
2023-08-15 13:32:40 +0300

    complete fourth task

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,3 +8,55 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti 
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
+
+const output = document.getElementById("output")
+
+function createCard(brand, models) {
+  const card = document.createElement("div")
+  card.classList.add("card")
+
+  const cardTitle = document.createElement("h2")
+  cardTitle.classList.add("card-title")
+  cardTitle.innerText = brand
+
+  const cardBody = document.createElement("div")
+  cardBody.classList.add("card-extended")
+
+  const cardExtender = document.createElement("button")
+  cardExtender.classList.add("card-extender")
+  cardExtender.innerText = "Car Models >"
+  cardExtender.addEventListener("click", () => {
+    cardBody.classList.toggle("card-extended")
+    cardExtender.innerText = cardBody.classList.contains("card-extended")
+      ? "Car Models >"
+      : "Car Models v"
+  })
+
+  const cardBodyList = document.createElement("ul")
+
+  for (const model of models) {
+    const cardBodyListItem = document.createElement("li")
+    cardBodyListItem.classList.add("card-body-list-item")
+    cardBodyListItem.innerText = model
+    cardBodyList.appendChild(cardBodyListItem)
+  }
+
+  cardBody.appendChild(cardBodyList)
+
+  card.appendChild(cardTitle)
+  card.appendChild(cardExtender)
+  card.appendChild(cardBody)
+
+  return card
+}
+
+async function main() {
+  const response = await fetch("cars.json")
+  const cars = await response.json()
+  cars.forEach((car) => {
+    const card = createCard(car.brand, car.models)
+    output.appendChild(card)
+  })
+}
+
+main()
