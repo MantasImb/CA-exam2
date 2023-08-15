400b71e6c1cd1be72a6aa5748934e9bdd88a8a14
Mantas Imbrasas <mantasimbrasas@gmail.com>
2023-08-15 14:19:56 +0300

    complete eighth task

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,25 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+  this.sum = function (a, b) {
+    return a + b
+  }
+  this.subtraction = function (a, b) {
+    return a - b
+  }
+  this.multiplication = function (a, b) {
+    return a * b
+  }
+  this.division = function (a, b) {
+    return a / b
+  }
+}
+
+const calc = new Calculator()
+
+console.log(calc.sum(2, 3))
+console.log(calc.subtraction(2, 3))
+console.log(calc.multiplication(2, 3))
+console.log(calc.division(2, 3))
