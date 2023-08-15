d75bc809d3328d2f22f5902d7b191448d315131c
Mantas Imbrasas <mantasimbrasas@gmail.com>
2023-08-15 08:19:19 +0300

    complete first task

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,26 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const form = document.querySelector("form")
+const output = document.querySelector("#output")
+
+function convertToLb(kgAmount) {
+  return kgAmount * 2.2046
+}
+function convertToG(kgAmount) {
+  return kgAmount / 0.001
+}
+function convertToOz(kgAmount) {
+  return kgAmount * 35.274
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  console.log(e.target)
+  const kgAmount = e.target[0].value
+  output.textContent = `
+    Svarai: ${convertToLb(kgAmount)};
+    Gramai: ${convertToG(kgAmount)};
+    Uncijos: ${convertToOz(kgAmount)}.
+  `
+})
