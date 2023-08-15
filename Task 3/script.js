310165b61d0fbe0aa27f63c30aed3dc6e39d91e7
Mantas Imbrasas <mantasimbrasas@gmail.com>
2023-08-15 08:45:32 +0300

    complete third task

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,34 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users"
+
+const button = document.getElementById("btn")
+const output = document.getElementById("output")
+const message = document.getElementById("message")
+
+button.addEventListener("click", async () => {
+  const response = await fetch(ENDPOINT)
+  const users = await response.json()
+
+  message.style.display = "none"
+
+  users.forEach((user) => {
+    // create card
+    const card = document.createElement("div")
+    card.classList.add("user-card")
+
+    // create image
+    const image = document.createElement("img")
+    image.src = user.avatar_url
+    image.alt = user.login
+    card.appendChild(image)
+
+    // create login
+    const login = document.createElement("p")
+    login.textContent = user.login
+    card.appendChild(login)
+
+    output.appendChild(card)
+  })
+})
